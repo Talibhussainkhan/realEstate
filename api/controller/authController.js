9b3af2e3978a7f22388b35d40f2a089160e757ff
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-14 19:35:33 +0500

    create sign in route

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -19,7 +19,6 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'User not found!'));
-        console.log(password)
         const validPassword = await bcryptjs.compare(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Wrong crendentials'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
