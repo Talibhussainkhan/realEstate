fb53b13b82b6b33092c04663bce70fb1aebb7139
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-16 19:29:59 +0500

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/store.js'
-
+import { persistor, store } from './store/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,13 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import  userReducer  from './userSlice/userSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({ user : userReducer });
+
+const rootPersistConfig = {
+    key: 'root',
+    storage: storage,
+    version : 1
+  }
+  
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer) 
+
 
 export const store = configureStore({
-  reducer: {
-    user : userReducer
-  },
+  reducer: persistedReducer,
   // not show any error on browser
   middleware : (getDefaultMiddleware)=>
     getDefaultMiddleware({
         serializableCheck : false,
     }),
 })
+
+export const persistor = persistStore(store);
